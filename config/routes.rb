fbc7daddc79d5ec8fd0c6b066bebf62a8beb0297
Raghu Betina <raghu@firstdraft.com>
2016-12-21 16:45:46 +0000

    Generate owners resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :owners
+  # Routes for the Owner resource:
+  # READ
+  get "/owners", :controller => "owners", :action => "index"
+  get "/owners/:id", :controller => "owners", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

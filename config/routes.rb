b5100ce04613d4f5f55787b8ea13533673ddba9b
Raghu Betina <raghu@firstdraft.com>
2016-12-21 16:46:09 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "owned_things#index"
   # Routes for the Owned_thing resource:
   # CREATE
